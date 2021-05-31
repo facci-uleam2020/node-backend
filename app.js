a9f0df67f5f8f44f271fa5f45ff7f41329dc3f06
facci-uleam2020 <84675845+facci-uleam2020@users.noreply.github.com>
2021-05-31 14:15:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Configure Header HTTP
+app.use(cors);
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
